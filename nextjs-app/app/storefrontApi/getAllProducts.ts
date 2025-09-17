9547b25c399554a85658afe3148c33a5a12085f3
Giorgiocig <giorgiocignitti@gmail.com>
2025-09-17 16:48:13 +0200

    Create getAllProduct fn

---
+++ nextjs-app/app/storefrontApi/getAllProducts.ts
@@ -0,0 +1,29 @@
+import { shopifyFetch } from "@/utils/fetch-shopify";
+const gql = String.raw;
+
+export async function getAllProducts() {
+  return shopifyFetch({
+    query: gql`
+      query ShopName {
+        products(first: 3) {
+          edges {
+            node {
+              title
+              priceRange {
+                minVariantPrice {
+                  amount
+                  currencyCode
+                }
+              }
+              images(first: 1) {
+                nodes {
+                  src
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+  });
+}
