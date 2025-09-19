169d10aaa5e98133ad7ace23c9ec62a06835e813
Giorgiocig <giorgiocignitti@gmail.com>
2025-09-19 17:16:38 +0200

    Update nextjs config file to include shopify cdn

--- nextjs-app/next.config.ts
+++ nextjs-app/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    domains: ["cdn.shopify.com"],
     remotePatterns: [
       {
         protocol: "http",
